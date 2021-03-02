ce5bc5624ca70fe7bbbd7ebe357a47dc80aaa38d
Cameron Collins <cameroncollins19@yahoo.com>
2021-03-02 00:22:19 -0600

    Added Static VectorMath Library and VectorMathTesting Console Project to Test the Library. Vector2D 45% finished

---
+++ VectorMath/VectorMath.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+namespace vm {
+
+	template <typename T>
+	struct Vector2d {
+	public:
+		T element_one;
+		T element_two;
+
+		Vector2d(T first, T second) : element_one(first), element_two(second) {
+
+		}
+
+		//Operator Overloading
+		Vector2d<T>* operator+(const Vector2d<T>& rhs) {
+			return &Vector2d<T>(rhs.element_one + element_one, rhs.element_two + element_two);
+		}
+
+		Vector2d<T>* operator-(const Vector2d<T>& rhs) {
+			return &Vector2d<T>(rhs.element_one - element_one, rhs.element_two - element_two);
+		}
+
+	};
+}
---
+++ VectorMathTesting/VectorMathTesting.cpp
@@ -0,0 +1,17 @@
+// VectorMathTesting.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <VectorMath.h>
+
+int main()
+{
+    vm::Vector2d<int> vector0(10, 20);
+    vm::Vector2d<int> vector1(5, 10);
+    std::cout << (vector0 + vector1)->element_one;
+    //(vector0 + vector1);
+    //std::cout << vector2->element_one << ", " << vector2->element_two << std::endl;
+
+
+
+}
